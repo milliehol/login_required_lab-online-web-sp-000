26f06f6f3856eca165eb00b03c5a78e327ec6472
Carla Holcomb <carla.programmer@gmail.com>
2020-05-07 17:05:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-
-  def current_user
-    session[:name]
-  end
-
   private
 
   def require_login
